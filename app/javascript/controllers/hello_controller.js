a91dedbfcf2b3280b8c6239eafd2cca670cdf9ae
JLODI <88990920+JLODI@users.noreply.github.com>
2025-01-31 02:40:44 -0800

    edited stimulus hello controller for testing if stimulus works

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["output"]
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log("Hello, Stimulus is working!");
+  }
+
+  greet() {
+    
+    this.outputTarget.textContent = "Hello, Stimulus!";
   }
 }
