bb5b934b5028042a461dccf4be8fc27aa8981c34
JLODI <88990920+JLODI@users.noreply.github.com>
2025-02-01 03:32:50 -0800

    added posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+  
+  def index
+      @posts = Post.all.limit(10)
+      @post = Post.new
+  end
+
+  def create
+      @post = current_user.posts.build(post_params)
+      if @post.save
+          flash[:notice] = "Your post has been created."
+          redirect_to posts_path
+      else
+      
+          flash[:alert] = @post.errors.full_messages[0]
+          redirect_to posts_path
+      end
+  end
+
+  def show
+      @photos = post.images
+  end
+
+  def edit
+
+  end
+
+  def destroy
+      @post.destroy
+      redirect_to posts_path
+      flash.now[:notice] = "Your post has been deleted"
+
+  end
+
+  private
+
+  def set_post
+      @post = Post.find_by id: params[:id]
+      return if @post
+      flash.now[:alert] = "Post does not exist."
+      redirect_to root_path
+  end
+
+  def post_params
+      params.require(:post).permit(:description, images: [])
+  end
+end
