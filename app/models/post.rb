6eac1934cc2f1784189af6a077bee53a43e47f4f
JLODI <88990920+JLODI@users.noreply.github.com>
2025-02-01 17:43:49 -0800

    added backend validations for post form and image upload validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,29 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images, dependent: :destroy
+
+  validates :description, presence: true, length: { minimum: 5, maximum: 1000 }
+  validate :acceptable_image
+
+  def acceptable_image
+    return unless images.attached?
+
+    acceptable_types = ["image/jpeg", "image/png", "image/webp" ]
+    
+    images.each do |image| 
+      # Check file size
+      if image.blob.byte_size > 1.megabyte
+        errors.add(:images, "#{image.filename} is too large (must be less than 1 mb)")
+      end
+  
+      # Check file type
+      unless acceptable_types.include?(image.content_type)
+        errors.add(:images, "#{image.filename} must be a JPEG, PNG, or WEBP")
+      end
+
+      unless images.count <= 20
+        errors.add(:images, 'You can only upload a maximum of 20 images')
+      end
+    end
+  end
 end
