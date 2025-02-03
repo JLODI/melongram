b3a7e9dadde44417de85c03dc4a1178724df1f5a
JLODI <88990920+JLODI@users.noreply.github.com>
2025-02-02 18:34:27 -0800

    added image preview when uploading multiple photos

--- app/javascript/controllers/new_post_form_controller.js
+++ app/javascript/controllers/new_post_form_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="new-post-form"
 export default class extends Controller {
-  static targets = ['selectFileButton', 'photoFile'];
+  static targets = ['preview', 'selectFileButton', 'photoFile'];
 
   connect() {
   }
@@ -13,7 +13,33 @@ export default class extends Controller {
     this.photoFileTarget.click();
   }
 
-  submitForm() {
-    this.element.submit();
+  preview(event) {
+    const files = event.target.files; // Get the selected files
+    if (files) {
+      // Clear the preview container before adding new previews
+      this.previewTarget.innerHTML = "";
+
+      Array.from(files).forEach((file) => {
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+          const img = document.createElement("img");
+          img.src = e.target.result; // Set the source of the image to the data URL
+          img.style.borderRadius = "20%"; // Apply rounded edges
+          img.style.width = "75px"; // Set the width
+          img.style.height = "75px"; // Set the height
+          img.style.margin = "8px" // Set the margins
+          img.style.objectFit = "cover"; // Ensure the image covers the area, maintaining its aspect ratio
+
+          this.previewTarget.appendChild(img); // Append the preview image to the container
+        };
+
+        reader.readAsDataURL(file); // Read the file as a data URL
+      });
+    }
   }
+
+  // submitForm() {
+  //   this.element.submit();
+  // }
 }
