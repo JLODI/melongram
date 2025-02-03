de5f70777ce04db2a02474dc401f4d8f059a857f
JLODI <88990920+JLODI@users.noreply.github.com>
2025-02-03 03:51:14 -0800

    added instagram style post date in index views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+
+    def time_of_post(post)
+        secs = (Time.now - post.updated_at).to_i 
+            if secs <= 60
+                return  "now"
+            elsif secs <= 3600
+                mins = (secs / 60 ).to_i
+                return "#{mins}m"
+            elsif secs <= 216000
+                hours = (secs / 3600)
+                return "#{hours}h"
+            else
+                return "i didnt make that value yet"
+            end
+    end
 end
