1a1387b3a30826868a3ce16453f37d7044e23665
JLODI <88990920+JLODI@users.noreply.github.com>
2025-02-04 20:22:40 -0800

    date helpers for post model

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,17 +1,77 @@
 module PostsHelper
 
-    def time_of_post(post)
-        secs = (Time.now - post.updated_at).to_i 
-            if secs <= 60
-                return  "now"
-            elsif secs <= 3600
-                mins = (secs / 60 ).to_i
-                return "#{mins}m"
-            elsif secs <= 216000
-                hours = (secs / 3600)
-                return "#{hours}h"
-            else
-                return "i didnt make that value yet"
-            end
+    def post_date_short(post)
+        secs = (Time.now.to_i - post.created_at.to_i) 
+        if secs <= 60 # minute
+            return  "now"
+        elsif secs <= (60 * 60) # hour
+            mins = (secs / 60 ).to_i
+            return "#{mins}m"
+        elsif secs <= (60 * 60 * 24) # day
+            hours = (secs / (60 * 60))
+            return "#{hours}h"
+        elsif secs <= (60 * 60 * 24 * 7) # week
+            days = (secs / (60 * 60 * 24))
+            return "#{days}d"
+        elsif secs <= (60 * 60 * 24 * 7 * 30) # month
+            weeks = (secs / (60 * 60 * 24 * 7))
+            return "#{weeks}w"
+        elsif secs <= (60 * 60 * 24 * 7 * 30 * 12) # year
+            months = (secs / (60 * 60 * 24 * 7 * 30 ))
+            return "#{months}m"
+        elsif secs > (60 * 60 * 24 * 7 * 30 * 12) # year
+            years = (secs / (60 * 60 * 24 * 7 * 30 * 12 ))
+            return "#{years}y"
+        else
+            return ""
+        end
+    end
+
+    def post_date_short_desc(post)
+        secs = (Time.now.to_i - post.updated_at.to_i) 
+        if secs <= 60 # minute
+            return  "now"
+        elsif secs <= (60 * 60) # hour
+            mins = (secs / 60 ).to_i
+            return "#{mins}m"
+        elsif secs <= (60 * 60 * 24) # day
+            hours = (secs / (60 * 60))
+            return "#{hours}h"
+        elsif secs <= (60 * 60 * 24 * 7) # week
+            days = (secs / (60 * 60 * 24))
+            return "#{days}d"
+        elsif secs <= (60 * 60 * 24 * 7 * 30) # month
+            weeks = (secs / (60 * 60 * 24 * 7))
+            return "#{weeks}w"
+        elsif secs <= (60 * 60 * 24 * 7 * 30 * 12) # year
+            months = (secs / (60 * 60 * 24 * 7 * 30 ))
+            return "#{months}m"
+        elsif secs > (60 * 60 * 24 * 7 * 30 * 12) # year
+            years = (secs / (60 * 60 * 24 * 7 * 30 * 12 ))
+            return "#{years}y"
+        else
+            return ""
+        end
+    end
+
+    def post_date_long(post)
+        secs = (Time.now.to_i - post.created_at.to_i) 
+        if secs <= 60 # minute
+            return  "now"
+        elsif secs <= (60 * 60) # hour
+            mins = (secs / 60 )
+            return "#{mins} #{"hour".pluralize(mins)} ago"
+        elsif secs <= (60 * 60 * 24) # day
+            hours = (secs / (60 * 60))
+            return "#{hours} #{"hour".pluralize(hours)} ago"
+        elsif secs >= (60 * 60 * 24) and secs < (60 * 60 * 24 * 2) # 2 days
+            return "yesterday"
+        elsif secs <= (60 * 60 * 24 * 7) # week
+            days = (secs / (60 * 60 * 24))
+            return "#{days} #{"day".pluralize(days)} ago"
+        else
+            date =  post.created_at.strftime("%B %d, %Y")
+            return date
+        end
     end
 end
