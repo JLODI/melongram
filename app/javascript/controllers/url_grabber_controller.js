b0fb7b6acc259fc2d8c2923e0f9c8d5166451300
JLODI <88990920+JLODI@users.noreply.github.com>
2025-02-06 03:25:08 -0800

    added copy post url to clipboard option

---
+++ app/javascript/controllers/url_grabber_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["url"]
+  connect() {
+    console.log(this.urlTarget);
+  }
+
+  copyLink(){
+    const url = this.urlTarget.getAttribute('data-post-url'); // Get the URL of the post
+
+     // copies the url
+    navigator.clipboard.writeText(url);
+
+    this.flashMessage();
+  }
+
+  flashMessage(){
+    // finds the flash container
+    let container = document.getElementById('stimulus-flash')
+
+    // append the message to the container
+    container.textContent = 'Link copied to the clipboard.';
+
+    // play slide in animation
+    container.classList.add("slide-in-out")
+
+  }
+}
