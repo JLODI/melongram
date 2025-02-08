0e124227584a4c689f9c824d36c7c0c06335dc62
JLODI <88990920+JLODI@users.noreply.github.com>
2025-02-08 02:00:09 -0800

    added likes route

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :posts
+  resources :likes, only: [:create, :destroy]
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
