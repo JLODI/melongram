b5ef7ea7e24d9a7fd5fae1bdfe58f787ed645a3a
JLODI <88990920+JLODI@users.noreply.github.com>
2025-02-12 17:13:59 -0800

    added show/hide replies button, need to fix text change later

--- app/javascript/controllers/comment_reply_controller.js
+++ app/javascript/controllers/comment_reply_controller.js
@@ -1,10 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["form"]
-  toggle(event) {
-    event.preventDefault()
-    this.formTarget.classList.toggle("d-none")
-  }
+    static targets = ["form", "replies","link"]
+    
+    connect(){
+    }
+
+    toggleForm(event) {
+        event.preventDefault()
+        this.formTarget.classList.toggle("d-none")
+    }
+    toggleReplies(event) {
+        event.preventDefault()
+        this.repliesTarget.classList.toggle("d-none")
+    }
+    changeText(event) {
+        event.preventDefault()
+        const linkText = this.linkTarget.textContent;
+            if (linkText.includes('show')) {
+            this.linkTarget.textContent = "Hide all replies";
+        }
+            else if (linkText.includes('hide')){
+            this.linkTarget.textContent = "Show all #{ pluralize(comment.comments.count, 'reply' )}";
+            console.log(this.linkTarget.textContent)
+            }
+    }
 
 }
